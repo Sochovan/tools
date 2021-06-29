47054a536d168e44394c054f06fdd3839e626d12
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-29 14:24:58 +0300

    test1

---
+++ lesson2/task1/dist/index.js
@@ -0,0 +1,10 @@
+// Babel
+import { printProfile } from "./profile";
+const userData = {
+  name: "Tom",
+  age: 17
+};
+const profile = { ...userData,
+  company: "Gromcode"
+};
+printProfile(profile);
