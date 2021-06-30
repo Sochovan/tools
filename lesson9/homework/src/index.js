c0e97979719eed4e5c24f7576081a6f7b35de42c
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-30 19:12:17 +0300

    test1

---
+++ lesson9/homework/src/index.js
@@ -0,0 +1,22 @@
+import { renderTasks } from "./list/renderer";
+import { initTodoListHandlers } from "./list/todoList";
+import { getTasksList } from "./list/tasksGateway";
+import { setItem } from "./list/storage";
+import "./index.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  getTasksList().then((tasksList) => {
+    setItem("tasksList", tasksList);
+    renderTasks();
+  });
+
+  initTodoListHandlers();
+});
+
+const onStorageChange = (e) => {
+  if (e.key === "tasksList") {
+    renderTasks();
+  }
+};
+
+window.addEventListener("storage", onStorageChange);
